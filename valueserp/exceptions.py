0e8f2f150b56228674963cce4424f7095143a991
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2022-07-04 17:00:01 +0100

    A whole bunch of docstrings

--- valueserp/exceptions.py
+++ valueserp/exceptions.py
@@ -1,10 +1,19 @@
+"""Exception classes.
+
+All exceptions inherit from the `VSException` base.
+"""
+
+
 class VSException(Exception):
+    """Base library exception."""
     pass
 
 
 class InvalidCredentials(VSException, ValueError):
+    """Provided credentials are not valid."""
     pass
 
 
 class APIError(VSException):
+    """The VALUE SERP API returned an error."""
     pass
