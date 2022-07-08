658025191266917c32d370f9075146d9f115c5bb
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2022-07-08 11:11:41 +0100

    Add error handling for connection errors, timeouts and other unknown issues (#5)

--- valueserp/client.py
+++ valueserp/client.py
@@ -18,7 +18,7 @@ import requests as requests
 from requests_toolbelt.sessions import BaseUrlSession
 
 from valueserp import const
-from valueserp.exceptions import APIError
+import valueserp.exceptions
 from valueserp.serp.web import WebSERP
 
 if TYPE_CHECKING:
@@ -77,21 +77,27 @@ class GoogleClient:
         if isinstance(params, dict):
             final_params.update(params)
 
-        res = self._session.request(request_type,
-                                    path,
-                                    params=final_params,
-                                    headers=headers,
-                                    json=data)
-        raw_json = res.json()
-
         try:
+            res = self._session.request(request_type,
+                                        path,
+                                        params=final_params,
+                                        headers=headers,
+                                        json=data)
+            raw_json = res.json()
             res.raise_for_status()
         except requests.exceptions.HTTPError as e:
             status_code = res.status_code
             message = raw_json['request_info'].get(
                 'message', 'No additional information.')
-            raise APIError(
+            raise valueserp.exceptions.APIError(
                 f'VALUE SERP API responded with status {status_code}: {message}'
             ) from e
+        except requests.exceptions.ConnectionError as e:
+            raise valueserp.exceptions.ConnectionError() from e
+        except requests.exceptions.Timeout as e:
+            raise valueserp.exceptions.Timeout() from e
+        except requests.exceptions.RequestException as e:
+            raise valueserp.exceptions.VSException(
+                'An unknown error occurred.') from e
 
         return raw_json
--- valueserp/exceptions.py
+++ valueserp/exceptions.py
@@ -2,18 +2,34 @@
 
 All exceptions inherit from the `VSException` base.
 """
+from __future__ import annotations
 
 
 class VSException(Exception):
-    """Base library exception."""
+    """Base package exception class."""
     pass
 
 
 class InvalidCredentials(VSException, ValueError):
-    """Provided credentials are not valid."""
+    """The provided credentials are invalid."""
     pass
 
 
 class APIError(VSException):
-    """The VALUE SERP API returned an error."""
+    """The VALUE SERP API responded with an error."""
     pass
+
+
+class ConnectionError(VSException):
+    """Failed to connect to the VALUE SERP API."""
+
+    def __init__(self, message: str | None = None):
+        super().__init__(message or 'Failed to connect to the VALUE SERP API.')
+
+
+class Timeout(VSException):
+    """Connection to the VALUE SERP API timed out."""
+
+    def __init__(self, message: str | None = None):
+        super().__init__(
+            message or 'Connection to the VALUE SERP API timed out.')
