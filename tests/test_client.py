9d14a7730806f09b22f86a2ddcb75a9f0dcbb46d
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2024-06-02 00:02:55 +0100

    Write new client tests

---
+++ tests/test_client.py
@@ -0,0 +1,49 @@
+from unittest import mock
+
+import pytest
+
+from valueserp import const
+from valueserp.client import GoogleClient
+from valueserp.credentials import Credentials
+from valueserp.serp import WebSERP
+
+
+@pytest.fixture(scope="module")
+async def client():
+    creds = Credentials('TESTKEY', auto_validate=False)
+    with GoogleClient(creds) as client:
+        yield client
+
+
+class TestGoogleClient:
+    @pytest.mark.asyncio
+    async def test_search_success(self, client: GoogleClient):
+        with mock.patch("valueserp.GoogleClient._request") as mock_request:
+            mock_request.return_value = '{"result": "success"}'
+            result = client.search({'q': 'test'})
+            mock_request.assert_called_once_with(
+                const.API_PATH["search"], params={'q': 'test'}
+            )
+            assert result == {'result': 'success'}
+
+
+    @pytest.mark.asyncio
+    async def test_web_search_success(self, client: GoogleClient):
+        with mock.patch("valueserp.GoogleClient.search") as mock_search:
+            mock_search.return_value = {'result': 'success'}
+            result = client.web_search('test')
+            mock_search.assert_called_once_with(params={'q': 'test', 'location': None})
+            assert isinstance(result, WebSERP)
+            assert result.raw == {'result': 'success'}
+
+
+    @pytest.mark.asyncio
+    async def test_site_web_search_success(self, client: GoogleClient):
+        with mock.patch("valueserp.GoogleClient.search") as mock_search:
+            mock_search.return_value = {'result': 'success'}
+            result = client.web_search('test', site='example.com')
+            mock_search.assert_called_once_with(
+                params={'q': 'site:example.com test', 'location': None},
+            )
+            assert isinstance(result, WebSERP)
+            assert result.raw == {'result': 'success'}
